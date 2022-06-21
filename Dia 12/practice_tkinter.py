95ef057fd3adb7751551f1356f3de15a8353fda2
JorgeGBravo <JorgeGBravo@users.noreply.github.com>
2022-06-21 18:18:31 +0100

    Dia 12 - tkinter - frame buttons

--- Dia 12/practice_tkinter.py
+++ Dia 12/practice_tkinter.py
@@ -24,64 +24,154 @@ left_panel.pack(side=LEFT)
 cost_panel = Frame(left_panel, bd=1, relief=FLAT)
 cost_panel.pack(side=BOTTOM)
 # food panel
-food_panel = LabelFrame(left_panel, text='Comida', font=('Dosis', 19, 'bold'),bd=1, relief=FLAT, fg='azure4')
+food_panel = LabelFrame(left_panel,
+                        text='Comida',
+                        font=('Dosis', 19, 'bold'),
+                        bd=1,
+                        relief=FLAT,
+                        fg='azure4')
 food_panel.pack(side=LEFT)
 # drink panel
-drink_panel = LabelFrame(left_panel, text='Bebidas', font=('Dosis', 19, 'bold'),bd=1, relief=FLAT, fg='azure4')
+drink_panel = LabelFrame(left_panel,
+                         text='Bebidas',
+                         font=('Dosis', 19, 'bold'),
+                         bd=1,
+                         relief=FLAT,
+                         fg='azure4')
 drink_panel.pack(side=LEFT)
 # desert panel
-desert_panel = LabelFrame(left_panel, text='Postres', font=('Dosis', 19, 'bold'),bd=1, relief=FLAT, fg='azure4')
+desert_panel = LabelFrame(left_panel,
+                          text='Postres',
+                          font=('Dosis', 19, 'bold'),
+                          bd=1,
+                          relief=FLAT,
+                          fg='azure4')
 desert_panel.pack(side=LEFT)
 
 # right panel
-right_panel = Frame(app, bd=1, relief=FLAT)
+right_panel = Frame(app, bd=1,
+                    relief=FLAT)
 right_panel.pack(side=RIGHT)
 # calc panel
-calc_panel = Frame(right_panel, bd=1, relief=FLAT, bg='burlywood')
+calc_panel = Frame(right_panel,
+                   bd=1,
+                   relief=FLAT,
+                   bg='burlywood')
 calc_panel.pack()
 # invoice panel
-invoice_panel = Frame(right_panel, bd=1, relief=FLAT, bg='burlywood')
+invoice_panel = Frame(right_panel,
+                      bd=1,
+                      relief=FLAT,
+                      bg='burlywood')
 invoice_panel.pack()
 # buttons panel
-buttons_panel = Frame(right_panel, bd=1, relief=FLAT, bg='burlywood')
+buttons_panel = Frame(right_panel,
+                      bd=1,
+                      relief=FLAT,
+                      bg='burlywood')
 buttons_panel.pack()
 
 # products list
-food_list = ['pollo', 'cordero', 'ternera', 'sardina', 'salmon', 'mero', 'kebab', 'pizza Margarita', 'chicharron' ]
-drinks_list = ['agua', 'refresco', 'soda', 'jugo', 'cafe', 'te', 'vino1','vino2', 'vodka']
+food_list = ['pollo', 'cordero', 'ternera', 'sardina', 'salmon', 'mero', 'kebab', 'pizza Margarita', 'chicharron']
+drinks_list = ['agua', 'refresco', 'soda', 'jugo', 'cafe', 'te', 'vino1', 'vino2', 'vodka']
 deserts_list = ['mouse', 'tartaleta', 'gelatina', 'flan', 'helado1', 'helado2', 'tarta1', 'tarta2', 'piña']
 
-# checkbuttons
-# itams foods
-var_food= []
+# items foods
+var_food = []
+frame_button = []
+text_food = []
 count = 0
 for food in food_list:
+
+    # make checkbuttons
     var_food.append('')
     var_food[count] = IntVar()
-    food = Checkbutton(food_panel, text=food.title(), font=('Dosis', 19, 'bold'), onvalue=1, offvalue=0, variable=var_food[count])
-    food.grid(row=count, column=0, sticky=W)
+    food = Checkbutton(food_panel, text=food.title(),
+                       font=('Dosis', 19, 'bold'), onvalue=1, offvalue=0,
+                       variable=var_food[count])
+    food.grid(row=count,
+              column=0,
+              sticky=W)
+
+    # make foods-buttons
+    frame_button.append('')
+    text_food.append('')
+    frame_button[count] = Entry(food_panel,
+                                font=('Dosis', 18, 'bold'),
+                                bd=1,
+                                width=4,
+                                state=DISABLED,
+                                textvariable=text_food[count])
+    frame_button[count].grid(row=count,
+                             column=1)
     count += 1
 
-# itams foods
+
+# items drinks
 var_drink = []
+frame_drink = []
+text_drink = []
 count = 0
 for drink in drinks_list:
+
+    # make checkbuttons
     var_drink.append('')
     var_drink[count] = IntVar()
-    drink = Checkbutton(drink_panel, text=drink.title(), font=('Dosis', 19, 'bold'), onvalue=1, offvalue=0, variable=var_drink[count])
-    drink.grid(row=count, column=0, sticky=W)
+    drink = Checkbutton(drink_panel,
+                        text=drink.title(),
+                        font=('Dosis', 19, 'bold'),
+                        onvalue=1,
+                        offvalue=0,
+                        variable=var_drink[count])
+    drink.grid(row=count,
+               column=0,
+               sticky=W)
+
+    # make frame drinks-buttons
+    frame_drink.append('')
+    text_drink.append('')
+    frame_button[count] = Entry(drink_panel,
+                                font=('Dosis', 18, 'bold'),
+                                bd=1,
+                                width=4,
+                                state=DISABLED,
+                                textvariable=text_drink[count])
+    frame_button[count].grid(row=count,
+                             column=1)
     count += 1
 
-# itams foods
+# items deserts
 var_desert = []
+frame_desert = []
+text_desert = []
 count = 0
 for desert in deserts_list:
+
+    # make checkbuttons
     var_desert.append('')
     var_desert[count] = IntVar()
-    desert= Checkbutton(desert_panel, text=desert.title(), font=('Dosis', 19, 'bold'), onvalue=1, offvalue=0, variable=var_desert[count])
-    desert.grid(row=count, column=0, sticky=W)
+    desert = Checkbutton(desert_panel,
+                         text=desert.title(),
+                         font=('Dosis', 19, 'bold'),
+                         onvalue=1,
+                         offvalue=0,
+                         variable=var_desert[count])
+    desert.grid(row=count,
+                column=0,
+                sticky=W)
+
+    # make frame deserts-buttons
+    frame_desert.append('')
+    text_desert.append('')
+    frame_button[count] = Entry(desert_panel,
+                                font=('Dosis', 18, 'bold'),
+                                bd=1,
+                                width=4,
+                                state=DISABLED,
+                                textvariable=text_desert[count])
+    frame_button[count].grid(row=count,
+                             column=1)
     count += 1
 
 # prevent it from closing the app
 app.mainloop()
-
