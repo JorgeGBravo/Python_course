08e0c171fcbc2dbe8c59ca868524ee59e5d717bd
JorgeGBravo <JorgeGBravo@users.noreply.github.com>
2022-06-27 11:59:42 +0100

    folder What - send msg whatsapp and image

--- What/init.py
+++ What/init.py
@@ -9,7 +9,7 @@ import time
 seconds = time.time() + 60
 
 date = datetime.fromtimestamp(seconds)
-pywhatkit.sendwhatmsg('+34630328749', 'Esto es una prueba', date.hour, date.minute)
-
+pywhatkit.sendwhatmsg('+34633328749', 'Esto es una prueba', date.hour, date.minute)
 time.sleep(5)
+pywhatkit.sendwhats_image("+910123356789", "Images/Hello.png")
 
